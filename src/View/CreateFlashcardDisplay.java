c0b06e6e600e8b427b86d921d6b53d1d6e535064
Isveydi <i.moreno26@ncf.edu>
2024-12-04 18:30:07 -0500

    Flashcard project

--- src/View/CreateFlashcardDisplay.java
+++ src/View/CreateFlashcardDisplay.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
+// Test ProjectMain
 public class CreateFlashcardDisplay {
     private JLabel questionLabel;
     private JTextField questionInput;
